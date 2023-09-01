5de41a51842af2e3534a2ce611c9df822b8ba141
재맵 <119744720+yookyung-lee@users.noreply.github.com>
2023-09-01 18:23:11 +0900

    #10809

---
+++ 알파벳찾기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        
+        int[] arr= new int[26];
+        
+        for (int i=0; i<arr.length; i++){
+           arr[i]=-1;
+        }
+        
+        String S=in.nextLine();
+        
+        for (int i=0; i<S.length(); i++){
+            char ch=S.charAt(i); //문자열에서 한 글자씩 담는다
+            
+            if (arr[ch-'a']==-1){
+                arr[ch-'a']=i;
+            }
+        }
+        
+        for (int num:arr){
+            System.out.print(num+" ");
+        }
+            
+            
+    }
+}
+        
